5b18fb0f10d5551c395e33d54343d38ab740a80e
Sergei <hovorovskyi@gmail.com>
2020-08-14 14:28:37 +0300

    feature/module-4-04-how-state-work: add onMarkImportant handler

--- 1_todo/10-module-4/src/components/todo-list-item/todo-list-item.js
+++ 1_todo/10-module-4/src/components/todo-list-item/todo-list-item.js
@@ -5,15 +5,20 @@ import './todo-list-item.scss';
 export default class TodoListItem extends Component {
 	state = {
 		done: false,
+		important: false,
 	};
 
 	onLabelClick = () => {
 		this.setState({ done: true });
 	};
 
+	onMarkImportant = () => {
+		this.setState({ important: true });
+	};
+
 	render() {
-		const { label, important = false } = this.props;
-		const { done } = this.state;
+		const { label } = this.props;
+		const { done, important } = this.state;
 
 		let classNames = 'todo-list-item';
 
@@ -21,24 +26,20 @@ export default class TodoListItem extends Component {
 			classNames += ' done';
 		}
 
-		const style = {
-			color: important ? 'steelblue' : 'black',
-			fontWeight: important ? 'bold' : 'normal',
-		};
+		if (important) {
+			classNames += ' important';
+		}
 
 		return (
 			<span className={classNames}>
-				<span
-					style={style}
-					className="todo-list-item-label"
-					onClick={this.onLabelClick}
-				>
+				<span className="todo-list-item-label" onClick={this.onLabelClick}>
 					{label}
 				</span>
 
 				<button
 					type="button"
 					className="btn btn-outline-success btn-sm float-right"
+					onClick={this.onMarkImportant}
 				>
 					<i className="fa fa-exclamation" />
 				</button>
