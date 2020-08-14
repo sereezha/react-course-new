b0195c500ada05870bdd13277fcab5e2eae8bd88
Sergei <hovorovskyi@gmail.com>
2020-08-14 15:03:49 +0300

    feature/module-4-09-react-app-data: create onToggleDone and onToggleImportant handlers

--- 1_todo/10-module-4/src/components/todo-list-item/todo-list-item.js
+++ 1_todo/10-module-4/src/components/todo-list-item/todo-list-item.js
@@ -3,26 +3,15 @@ import React, { Component } from 'react';
 import './todo-list-item.scss';
 
 export default class TodoListItem extends Component {
-	state = {
-		done: false,
-		important: false,
-	};
-
-	onLabelClick = () => {
-		this.setState(({ done }) => {
-			return { done: !done };
-		});
-	};
-
-	onMarkImportant = () => {
-		this.setState(({ important }) => {
-			return { important: !important };
-		});
-	};
-
 	render() {
-		const { label, onDeleted } = this.props;
-		const { done, important } = this.state;
+		const {
+			done,
+			label,
+			important,
+			onDeleted,
+			onToggleDone,
+			onToggleImportant,
+		} = this.props;
 
 		let classNames = 'todo-list-item';
 
@@ -36,22 +25,22 @@ export default class TodoListItem extends Component {
 
 		return (
 			<span className={classNames}>
-				<span className="todo-list-item-label" onClick={this.onLabelClick}>
+				<span className="todo-list-item-label" onClick={onToggleDone}>
 					{label}
 				</span>
 
 				<button
 					type="button"
 					className="btn btn-outline-success btn-sm float-right"
-					onClick={this.onMarkImportant}
+					onClick={onToggleImportant}
 				>
 					<i className="fa fa-exclamation" />
 				</button>
 
 				<button
 					type="button"
-          className="btn btn-outline-danger btn-sm float-right"
-          onClick={onDeleted}
+					className="btn btn-outline-danger btn-sm float-right"
+					onClick={onDeleted}
 				>
 					<i className="fa fa-trash-o" />
 				</button>
