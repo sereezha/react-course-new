9b0700baf33d9985425b2ecb24b01510c3e1113c
Sergei <hovorovskyi@gmail.com>
2020-08-23 15:34:08 +0300

    feature/stardb-04-create-api-component

--- 2_star-db/src/components/random-planet/random-planet.js
+++ 2_star-db/src/components/random-planet/random-planet.js
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
 
 const RandomPlanet = () => {
+	const [planet, setPlanet] = useState({
+		id: null,
+		name: null,
+		population: null,
+		rotationPeriod: null,
+		diameter: null,
+	});
 
+	const swapiService = new SwapiService();
+
+	const updatePlanet = () => {
+		const someID = Math.floor(Math.random() * 25) + 2;
+		swapiService.getPlanet(7).then((planet) => {
+			setPlanet({
+				id: someID,
+				name: planet.name,
+				population: planet.population,
+				rotationPeriod: planet.rotation_period,
+				diameter: planet.diameter,
+			});
+		});
+	};
+
+	useEffect(() => {
+		updatePlanet();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const { id, name, population, rotationPeriod, diameter } = planet;
 	return (
 		<div className="random-planet jumbotron rounded">
-			<img
-				className="planet-image"
-        src={`https://starwars-visualguide.com/assets/img/planets/5.jpg`}
-        alt=""
-			/>
+			{!!id && (
+				<img
+					className="planet-image"
+					src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+					alt=""
+				/>
+			)}
 			<div>
-				<h4>Planet Name</h4>
+				<h4>{name}</h4>
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item">
 						<span className="term">Population</span>
-						<span>123124</span>
+						<span>{population}</span>
 					</li>
 					<li className="list-group-item">
 						<span className="term">Rotation Period</span>
-						<span>43</span>
+						<span>{rotationPeriod}</span>
 					</li>
 					<li className="list-group-item">
 						<span className="term">Diameter</span>
-						<span>100</span>
+						<span>{diameter}</span>
 					</li>
 				</ul>
 			</div>
