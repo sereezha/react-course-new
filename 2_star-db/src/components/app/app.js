ba47f9a91138472c7f847ae68558b0d7e1f2955b
Sergei <hovorovskyi@gmail.com>
2020-08-24 09:41:04 +0300

    feature/react-patterns-03-elements-properties: create Row component

--- 2_star-db/src/components/app/app.js
+++ 2_star-db/src/components/app/app.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ErrorButton from '../error-button';
 import PeoplePage from '../people-page';
 
 import './app.css';
